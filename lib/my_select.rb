5909618e3883c6cea21d62c5e81edeb5cf9a390e
Eric Razor <eric.razorjr@gmail.com>
2018-05-03 05:48:24 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  newset = []
  counter = 0 
  while counter < collection.size do
-   if array[counter] ==true
+   if yield(array[counter]) ==true
      newset.push(array[counter])
    end
    counter += 1 
